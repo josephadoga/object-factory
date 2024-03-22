28dfaa7bd5ee31fe999a9f6d955610526d20205a
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-22 14:00:13 -0500

    Finished HTML for now and Started JavaScript

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,11 @@
+import shape from './Shape.js';
 'use strict';
+
+function listen(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+  
+function select(selector) {
+    return document.querySelector(selector);
+}
+
