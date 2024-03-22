0fd240c3b53fd425da1ad6967c8d53ccc02c827d
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-22 16:09:32 -0500

    At least, I have done something.

---
+++ assets/script/Shape.js
@@ -0,0 +1,21 @@
+class Shape {
+    #name;
+    #colour;
+
+    constructor(name, colour) {
+        this.#name = name;
+        this.#colour = colour;
+    }
+
+    set name(name) { this.#name = name; }
+    get name() { return this.#name; }
+
+    set colour(colour) { this.#colour = colour; }
+    get colour() { return this.#colour; }
+
+    getInfo() {
+        return `${this.#name} ${this.#colour}`;
+    }
+}
+
+export default Shape;
--- assets/script/app.js
+++ assets/script/app.js
@@ -1,4 +1,4 @@
-import shape from './Shape.js';
+import Shape from './Shape.js';
 'use strict';
 
 function listen(event, selector, callback) {
@@ -8,4 +8,48 @@ function listen(event, selector, callback) {
 function select(selector) {
     return document.querySelector(selector);
 }
+
+const inputShape = select('.shape');
+const inputColour = select('.colour');
+const selectButton = select('.create');
+const gridBox = select('.grid-box');
+const messageBox = select('.message');
+const div = select('div');
+
+figureArray = []''
+
+function createShape() {
+    const shape = inputShape.value;
+    const colour = inputColour.value;
+
+    const figure = new Shape(shape, colour);
+
+    if (shape && colour) {
+        // gridBox.innerHTML += `<div class="figure-square"></div>`;
+        // gridBox.style.backgroundColor = colour;
+
+        const newDiv = document.createElement('div');
+        newDiv.className = `figure-${shape}`;
+        newDiv.style.backgroundColor = colour;
+        gridBox.appendChild(newDiv);
+    }
+
+    // gridBox.innerHTML += `<div class="figure-square"></div>`;
+    // figureSquare.style.backgroundColor = colour;
+
+    // const newDiv = document.createElement('div');
+    // newDiv.className = 'figure-square';
+    // newDiv.style.backgroundColor = colour;
+    // gridBox.appendChild(newDiv);
+}
+
+listen('click', selectButton, () => {
+    createShape();
+    messageBox.innerText = '';
+});
+
+listen('click', div, () => {
+    createShape();
+    messageBox.innerText = '';
+});
 
